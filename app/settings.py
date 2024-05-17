7b572bb0a571c75591fb318ded023ee4f71c2ad9
Patrick Onduto <45040687+PatrickNdieka@users.noreply.github.com>
2024-05-17 16:40:33 +0300

    prep for deployment of static

--- app/settings.py
+++ app/settings.py
@@ -152,7 +152,12 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    # White noise settings
+    STORAGES = {
+        'staticfiles': {
+            'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        },
+    }
 
 
 MEDIA_URL = 'media/'
@@ -162,13 +167,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# White noise settings
-STORAGES = {
-    'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
-    },
-}
 
 # Email setting
 EMAIL_BACKEND = config('EMAIL_BACKEND')
